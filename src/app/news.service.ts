5824e7fd6ea157f54b939ee0b2179eec162b202a
Tomas Pulenta - Hx <tpulenta@gmail.com>
2018-09-26 11:06:50 -0300

    ojoalpiojo

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -16,7 +16,7 @@ export class NewsService {
   }
 
   getNews(theme, begin_date, end_date): Observable<Article[]> {
-    environment.articlesUrl = 'https://localhost:5001/news'; //esto es porque sino se pone dos veces 
+   // environment.articlesUrl = 'https://localhost:5001/news'; //esto es porque sino se pone dos veces 
     environment.articlesUrl += `?theme=${theme}&begin_date=${begin_date}&end_date=${end_date}`;
 
     return this.http.get<Article[]>(environment.articlesUrl).pipe(
@@ -25,11 +25,6 @@ export class NewsService {
   }
   private handleError<T>(operation = 'operation', result?: Article[]) {
     return (error: HttpErrorResponse): Observable<Article[]> => {
-      // console.log("***")
-      // console.log("***")
-      // console.log(console.log(error.status));
-      // console.log("***")
-      // console.log("***")
       this.validateErrors(error.status);
 
       return of(result as Article[]);
@@ -38,14 +33,15 @@ export class NewsService {
   private validateErrors(error) {
     switch (error) {
       case 404:
-      alert("Internal problem: The Web API's url is wrong ");
-
+        alert("Internal problem: The Web API's url is wrong ");
         break;
       case 500:
       case 0:
-      alert ("The web api is down. You should talk to the web api's administrator ");
+        alert("The web api is down. You should talk to the web api's administrator ");
         break;
-
+      case 503:
+        alert("The New York Times API is down.");
+        break;
       default:
         alert('There is a weird error, try again later..');
         break;
